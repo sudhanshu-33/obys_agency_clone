a6b7b0f4a1930fbc5eff9fb186b0ffe9e7aa630f
sudhanshu mehta <sk3360240@gmail.com>
2025-07-14 02:30:10 +0530

    page1 completed

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline();
+function loaderAnimation(){
+  var tl = gsap.timeline();
 
 tl.from(".element h1", {
   y: 150,
@@ -27,7 +28,7 @@ tl.to(".element h2",{
 tl.to("#loader", {
   opacity: 0,
   duration: 0.4,
-  delay: 3,
+  delay: 1,
 });
 tl.from("#page1",{
     delay:0.2,
@@ -39,3 +40,27 @@ tl.from("#page1",{
 tl.to("#loader",{
     display:"none"
 })
+tl.from("#page1 h1",{
+opacity:0,
+})
+tl.from("#heading1 h1,#heading2 h1,#heading3 h2,#heading4 h1",{
+  y:120,
+  stagger:0.2,
+  duration:0.7,
+})
+}
+
+
+function cursorAnimation(){
+document.addEventListener("mousemove",function(dets){
+ gsap.to("#cursr",{
+  left:dets.x,
+  top:dets.y,
+  
+ })
+})
+Shery.makeMagnet("#page1 #nav h4,.brand__svg", {
+});
+}
+loaderAnimation();
+cursorAnimation();
